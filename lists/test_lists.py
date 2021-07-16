e448c1da5c21cb24d284af73deecdd8c03751255
Kyle Guerrero <kyleguerrero85@gmail.com>
2021-07-16 01:08:03 -0500

    wip(testing): added test case for getting all the car years

--- lists/test_lists.py
+++ lists/test_lists.py
@@ -307,6 +307,63 @@ class TestLists(unittest.TestCase):
         actual = self.sort_cars(self.inventory)
         self.assertListEqual(expected, actual)
 
+    def test_get_car_years(self):
+        expected = [
+            2009,
+            2001,
+            2010,
+            1983,
+            1990,
+            1995,
+            2009,
+            1987,
+            1996,
+            2000,
+            2004,
+            2004,
+            1997,
+            1999,
+            2000,
+            2001,
+            1987,
+            1995,
+            1994,
+            1985,
+            2003,
+            1997,
+            1992,
+            2003,
+            2005,
+            2005,
+            2000,
+            2005,
+            1993,
+            2010,
+            1964,
+            1999,
+            2011,
+            1991,
+            2000,
+            2003,
+            1997,
+            1992,
+            1998,
+            2012,
+            1965,
+            1996,
+            2009,
+            2012,
+            2008,
+            1995,
+            2007,
+            2008,
+            1996,
+            1999,
+        ]
+
+        actual = self.get_car_years(self.inventory)
+        self.assertCountEqual(actual, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
