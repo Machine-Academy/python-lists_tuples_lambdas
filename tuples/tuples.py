48de7c0f37c8400ab8fae7fa29acaefa19f82f76
Kyle Guerrero <kyleguerrero85@gmail.com>
2021-07-16 21:19:37 -0500

    problem set(tuples): created problem set for tuples

--- tuples/tuples.py
+++ tuples/tuples.py
@@ -1,69 +1,72 @@
 # Let's get some practice using tuples.
+from collections import namedtuple
+
+
+def main():
+    interns = [
+        {"id": 1, "email": "mmelloy0@psu.edu", "name": "Mitzi", "gender": "F"},
+        {"id": 2, "email": "kdiben1@tinypic.com", "name": "Kennan", "gender": "M"},
+        {"id": 3, "email": "kmummery2@wikimedia.org", "name": "Keven", "gender": "M"},
+        {"id": 4, "email": "gmartinson3@illinois.edu", "name": "Gannie", "gender": "M"},
+        {"id": 5, "email": "adaine5@samsung.com", "name": "Antonietta", "gender": "F"},
+    ]
+
+    # === **Scoll down to challenege 1 ** ===
+    intern_tuples = create_intern_tuples(interns)
+
+    # ==== Challenge 2: Reading Tuple Data ====
+    # Once your tuples are created, print out the following requests from HR :
+    #   - Mitzi's name
+    #   - Kennan's ID
+    #   - Keven's email
+    #   - Gannie's name
+    #   - Antonietta's Gender
+
+    # === **Scroll down to challenge 3** ===
+    intern_named_tuples = create_named_tuples(interns)
+
+    # === Challenge 4 ===
+    # Once your **named tuples** are created, print out the following requests from HR into the console:
+
+    # Mitzi's name
+    # Kennan's ID
+    # Keven's email
+    # Gannie's name
+    # Antonietta's Gender
+
 
 # ==== Challenge 1: Creating Tuples ====
 # HR needs some information on the new interns put into a database.
-# Given an id, email, first name, and gender. Create an object for each person in the company list:
+# Given an id, email, first name, and gender. Create an tuple for each person in the company list:
 
-# 1, mmelloy0@psu.edu, Mitzi, F
-# 2, kdiben1@tinypic.com, Kennan, M
-# 3, kmummery2@wikimedia.org, Keven, M
-# 4, gmartinson3@illinois.edu, Gannie, M
-# 5, adaine5@samsung.com, Antonietta, F
+# Example format of an intern dict: {"id": 1, "email": "mmelloy0@psu.edu", "name": "Mitzi", "gender": "F"}
+def create_intern_tuples(interns_list):
+    interns = []
 
-# Example format of an intern object: 1, examples@you.edu, Example, F
-example = (0, "Example", "examples@you.edu", "F")
+    return interns
 
-# Write your tuples here:
-
-
-# ==== Challenge 2: Reading Object Data ====
-# Once your tuples are created, print out the following requests from HR into the console:
-
-# Mitzi's name
-
-# Kennan's ID
-
-# Keven's email
-
-# Gannie's name
-
-# Antonietta's Gender
 
 # ==== Challenge 3: Named Tuples ====
 # Pickig out data from a tuple can be cumbersome, esp when we have lots of fields in our data.
 # However we can make things easier using a **named tuple**. Named tuples allow us to
 # assign names to the values in our tuples.
-from collections import namedtuple
 
 # example
-color = namedtuple("Color", "r g b")
-
-#                 r    g    b
-my_color = color(255, 128, 78)
-print(my_color.r)
-print(my_color.g)
-print(my_color.b)
+# color = namedtuple("Color", "r g b")
+#                   r    g    b
+# my_color = color(255, 128, 78)
+# print(my_color.r)
+# print(my_color.g)
+# print(my_color.b)
 
 # reusing the data that HR gave us, create named tuples
+# Example format of an intern dict: {"id": 1, "email": "mmelloy0@psu.edu", "name": "Mitzi", "gender": "F"}
+def create_named_tuples(interns_list):
+    interns = []
 
-# 1, mmelloy0@psu.edu, Mitzi, F
-# 2, kdiben1@tinypic.com, Kennan, M
-# 3, kmummery2@wikimedia.org, Keven, M
-# 4, gmartinson3@illinois.edu, Gannie, M
-# 5, adaine5@samsung.com, Antonietta, F
-
-# === Challenge 4 ===
-# Once your **name tuples** are created, print out the following requests from HR into the console:
-
-# Mitzi's name
-
-# Kennan's ID
-
-# Keven's email
-
-# Gannie's name
-
-# Antonietta's Gender
+    return interns
 
 
-# === Great work! === Head over to the the lists.py!
+if __name__ == "__main__":
+    # call your main function
+    pass
